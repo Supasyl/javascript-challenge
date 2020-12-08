50ef55e1d4ec29baaa12525338b54694e525ae34
Supasyl <69235823+Supasyl@users.noreply.github.com>
2020-12-08 16:34:46 +0800

    working date search

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,12 +1,10 @@
 // assign data from 'data.js' to variable
 let sightings = data;
-console.log(sightings);
 
 // variables to reference parts of the table
-let table = d3.select('table');
 let tbody = d3.select('tbody');
 
-// filling the initial table with all values
+//// filling the initial table with all values
 sightings.forEach((sighting) => {
 	// add one row for each sighting
 	let row = tbody.append('tr');
@@ -19,31 +17,50 @@ sightings.forEach((sighting) => {
 	});
 });
 
-// filter with date input
+//// filter with date input
 //assign variable to inputfield
 function Action() {
-	//prevent the page from refreshing if enter is pressed
-	d3.event.preventDefault(); 
-	let dateField = d3.select('#Date');
+	// clear values in the current table
+	tbody.html('');
+	// define the input text
+	let dateField = d3.select('#datetime');
 	let dateInput = dateField.property("value");
-	// change the date column from string to date value (to yyyy/mm/dd, or field input to d/m/yyyy)
-	let newDateInput = new Date(d/M/yyyy).equals(Date.parseString(dateInput, 'yyyy,MM,dd'));
-	let dateSummery = sightings.filter(sighting => sighting.datetime === newDateInput);
-	console.log(dateInput, newDateInput, dateSummery);
+	let dateSummery = sightings.filter(sighting => sighting.datetime === dateInput);
+	// making a new table with filtered values
+	dateSummery.forEach((summery) => {
+		// add one row for each sighting
+		let row = tbody.append('tr');
+		// log each entry in the data
+		Object.entries(summery).forEach(([key, value]) => {
+			// append one cell for each value
+			let cell = row.append('td');
+			// add values to the cells
+			cell.text(value);
+		});
+	});
 };
-// // date field action function
-// let dateAction = dateInput.on('change', () => {
-	
-// 	// define the value that is input
-// 	let dateInput = d3.event.target.value;
-	
-// new Date(2000,0,2).equals(Date.parseString('1/2/2000','M/d/yyyy'))
-
-
 
 // when pressing submit button, use function
 let submitButton = d3.select('#button');
 submitButton.on('click', Action);
+// runEnter.on('keypress', Action);
 
+//prevent the page from refreshing if enter or spacebvar is pressed
 
+// input.addEventListener('keyup', (event) => {
+// 	if (event.keyCode === 13) {
+// 		event.preventDefault();
+// 		ubmitButton.on('click', Action);
+// 	}
+// };
 
+// let body = d3.select("body");
+// let p = body.selectAll(null)
+//   .on("keypress", () => {
+//     if(d3.event.keyCode === 13){
+//     console.log("Congrats, you pressed enter or space!")
+//     }
+// });
+
+// let runEnter = (d3.event.keyCode === 13);
+// runEnter.on('click', Action);
