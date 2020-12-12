84fbd9fee010042c3cc7aa40bb7275ddb0c17d8f
Supasyl <69235823+Supasyl@users.noreply.github.com>
2020-12-12 13:08:48 +0800

    Final level 2
    All fields work individually now

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -15,25 +15,30 @@ let countryList = [];
 let shapeList = [];
 
 //// filling the initial table with all values
-sightings.forEach((sighting) => {
-	// add one row for each sighting
-	let row = tbody.append('tr');
-	// log each entry in the data
-	Object.entries(sighting).forEach(([key, value]) => {
-		// append one cell for each value
-		let cell = row.append('td');
-		// add values to the cells
-		cell.text(value);
-		// list of Cities for the drop down list
-		if (key === 'city') {citiesList.push(value)};
-		// list of States for the drop down list
-		if (key === 'state') {stateList.push(value)};
-		// list of Countries for the drop down list
-		if (key === 'country') {countryList.push(value)};
-		// list of Shapes for the drop down list
-		if (key === 'shape') {shapeList.push(value)};
+function buildTable() {
+	tbody.html('');	
+	sightings.forEach((sighting) => {
+		// add one row for each sighting
+		let row = tbody.append('tr');
+		// log each entry in the data
+		Object.entries(sighting).forEach(([key, value]) => {
+			// append one cell for each value
+			let cell = row.append('td');
+			// add values to the cells
+			cell.text(value);
+			// list of Cities for the drop down list
+			if (key === 'city') {citiesList.push(value)};
+			// list of States for the drop down list
+			if (key === 'state') {stateList.push(value)};
+			// list of Countries for the drop down list
+			if (key === 'country') {countryList.push(value)};
+			// list of Shapes for the drop down list
+			if (key === 'shape') {shapeList.push(value)};
+		});
 	});
-});
+};
+
+buildTable();
 
 // sort lists in alphabetical order
 citiesList.sort();
@@ -98,28 +103,29 @@ function Action() {
 	d3.event.preventDefault();
 	// clear values in the current table
 	tbody.html('');	
+	let updatedSightings = sightings;
 	// define date field name and input text and filter search
 	let dateField = d3.select('#datetime');
 	let dateInput = dateField.property("value");
-	let dateSummery = sightings.filter(sighting => sighting.datetime === dateInput);
+	if (dateInput != '') {updatedSightings = updatedSightings.filter(sighting => sighting.datetime === dateInput)};
 	// define city dropdown field name and input and filter date search
 	let cityField = d3.select('#City');
 	let cityInput = cityField.property('value');
-	let citySummery = dateSummery.filter(sighting => sighting.city === cityInput);
+	if (cityInput != '') {updatedSightings = updatedSightings.filter(sighting => sighting.city === cityInput)};
 	// define state dropdown field name and input and filter date and city search
 	let stateField = d3.select('#State');
 	let stateInput = stateField.property('value');
-	let stateSummery = citySummery.filter(sighting => sighting.state === stateInput);
+	if (stateInput != '') {updatedSightings = updatedSightings.filter(sighting => sighting.state === stateInput)};
 	// define country dropdown field name and input and filter date, city and state search
 	let countryField = d3.select('#Country');
 	let countryInput = countryField.property('value');
-	let countrySummery = stateSummery.filter(sighting => sighting.country === countryInput);
+	if (countryInput != '') {updatedSightings = updatedSightings.filter(sighting => sighting.country === countryInput)};
 	// define shape dropdown field name and input and filter date, city, state and shape search
 	let shapeField = d3.select('#Shape');
 	let shapeInput = shapeField.property('value');
-	let shapeSummery = countrySummery.filter(sighting => sighting.shape === shapeInput);
+	if (shapeInput != '') {updatedSightings = updatedSightings.filter(sighting => sighting.shape === shapeInput)};
 	// making a new table with filtered values 
-	shapeSummery.forEach((summery) => {
+	updatedSightings.forEach((summery) => {
 		// add one row for each sighting
 		let row = tbody.append('tr');
 		// log each entry in the data
@@ -136,3 +142,6 @@ function Action() {
 let submitButton = d3.select('#button');
 let form = d3.select('#searchForm');
 form.on('submit', Action);
+
+let resetButton = d3.select('#button2');
+form.on('reset', buildTable);
